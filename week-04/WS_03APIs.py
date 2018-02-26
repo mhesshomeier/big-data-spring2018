4560747945b5dc4ad3f7135e0313e6417873c0c7
mhesshomeier <mjhh@mit.edu>
2018-02-26 11:01:03 -0500

    notes from wk 3 plus supporting documentation

---
+++ week-04/WS_03APIs.py
@@ -0,0 +1,11 @@
+In Atom, make a new .py file where we will be writing our scraper and import the libraries:
+
+```python
+import jsonpickle
+import tweepy
+import pandas as pd
+
+# Imports the keys from the python file
+# You may need to change working directory
+import os
+os.chdir('week-04')
